609e94485a8a9744696fc9cb250935d148b709c0
Django <gorshkov.artem2010@yandex.ru>
2025-03-06 15:01:10 +0300

    platform service fix3

---
+++ PlatformService/Profiles/PlatformProfiles.cs
@@ -0,0 +1,22 @@
+using AutoMapper;
+using PlatformService.Models;
+using PlatformService.Dtos;
+using Microsoft.AspNetCore.Routing.Constraints;
+
+namespace PlatformService.Dtos
+{
+    public class PlatformProfile : Profile
+    {
+        public PlatformProfile()
+        {
+            // Source -> Target
+            CreateMap<Platform, PlatformReadDto>();
+            CreateMap<PlatformCreateDto, Platform>();
+            CreateMap<PlatformReadDto, PlatformPublishedDto>();
+            CreateMap<Platform, GrpcPlatformModel>()
+                .ForMember(dest => dest.PlatformId, opt=> opt.MapFrom(src =>src.Id))
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
+                .ForMember(dest => dest.Publisher, opt => opt.MapFrom(src => src.Publisher));
+        }
+    }
+}
---
+++ PlatformService/Startup.cs
@@ -0,0 +1,81 @@
+﻿using System.Configuration;
+using System.Windows.Input;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using PlatformService.AsyncDataService;
+using PlatformService.Data;
+using PlatformService.SyncDataServices;
+using PlatformService.SyncDataServices.Http;
+
+
+namespace PlatformService
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        private readonly IWebHostEnvironment _env;
+
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
+        {
+            Configuration = configuration;
+            _env = env;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            if (_env.IsProduction())
+            {
+                Console.WriteLine("--> Using SQLServer DB");
+                services.AddDbContext<AppDbContext>(opt=>
+                opt.UseSqlServer(Configuration.GetConnectionString("PlatformsConn")));
+            }
+            else
+            {
+                Console.WriteLine("--> Using InMem DB");
+                services.AddDbContext<AppDbContext>(opt =>
+                    opt.UseInMemoryDatabase("InMem"));
+            }
+            services.AddScoped<IPlatformRepo, PlatformRepo>();
+            services.AddHttpClient<ICommandDataClient, HttpCommandDataClient>();
+            services.AddSingleton<IMessageBusClient, MessageBusClient>();
+            services.AddGrpc();
+            
+            services.AddControllers();
+            //services.AddAuthorization();
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "PlatformService", Version = "v1" });
+            });
+            Console.WriteLine($"--> CommandService Endpoint {Configuration["CommandService"]}");
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json","PlatformService"));
+            }
+            app.UseHttpsRedirection();
+            app.UseRouting();
+
+            app.UseAuthorization();
+            
+            app.UseEndpoints(Endpoint =>
+            {
+                Endpoint.MapControllers();
+                Endpoint.MapGrpcService<GrpcPlatformService>();
+
+                Endpoint.MapGet("/protos/platforms.proto", async context =>
+                {
+                    await context.Response.WriteAsync(File.ReadAllText("Protos/platforms.proto"));
+                });
+            });
+
+            PrepDb.PrepPopulation(app, env.IsProduction());
+        }
+    }
+}
